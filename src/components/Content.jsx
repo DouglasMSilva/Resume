fb851f72a765302d530498b474fcf9c26ffe7bf9
DouglasMSilva <melosilvadouglas7@gmail.com>
2025-11-01 11:36:43 -0300

    Evolved

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,7 +6,7 @@ export const Content = () => {
       <div>
         <h1>Resumo Profissional</h1>
         <p>Desenvolvedor Front-End com foco na criação de <strong>interfaces modernas</strong>, <strong>responsivas</strong> e <strong>intuitivas</strong>. Experiência prática no desenvolvimento e manutenção de sistemas web
-        utilizando linguagens como <strong>HTML</strong>, <strong>CSS</strong> e <strong>JavaScript</strong>, frameworks como <strong>Bootstrap</strong>, <strong>React</strong>, <strong>React Native</strong> utilizando hooks como useState e useEffect e conceitos de banco de dados com linguagem <strong>SQL</strong> através de serviços como o <strong>Supabase</strong>. Proativo, detalhista e comprometido com a entrega de soluções eficientes e escaláveis. Recentemente iniciei o curso de <strong>UX Research/UI Design</strong>(Experiência de Usuário) e estudos de conceitos de Design com <strong>Inkscape</strong>.</p>
+        utilizando linguagens e tecnologias como <strong>HTML</strong>, <strong>CSS</strong>, <strong>JavaScript</strong>, <strong>Nodejs</strong> e <strong>TypeScript</strong>, frameworks como <strong>Bootstrap</strong>, <strong>Express</strong>, <strong>React</strong>, <strong>React Native</strong> utilizando hooks como useState e useEffect e conceitos de banco de dados com linguagem <strong>SQL</strong> através de serviços como o <strong>Supabase</strong> e <strong>MongoDB</strong>. Proativo, detalhista e comprometido com a entrega de soluções eficientes e escaláveis. Recentemente iniciei o curso de <strong>UX Research/UI Design</strong>(Experiência de Usuário) e estudos de conceitos de Design com <strong>Inkscape</strong>.</p>
 
         <Skills />
       </div>
